e79e000a4830e4ab6ebce770646c8ec9b96595bb
gladosfromaperturelabs <35615186+gladosfromaperturelabs@users.noreply.github.com>
2018-02-05 00:15:07 +0100

    Update 20-firstboot.sh

--- 20-firstboot.sh
+++ 20-firstboot.sh
@@ -17,11 +17,11 @@ sudo pacman -Syyu
 sudo pacman-optimize
 
 # Install Basic Mate Desktop and Apps
-sudo pacman -S network-manager-applet xorg-xinit xorg-server xdg-user-dirs-gtk nvidia opencl-nvidia ocl-icd lib32-nvidia-utils  lib32-opencl-driver lib32-ocl-icd firefox hexchat geary chromium firefox-i18n-es-es perl-json-xs perl-term-readline-gnu pulseaudio budgie-desktop arc-solid-gtk-theme adapta-gtk-theme papirus-icon-theme gtk-engine-murrine gtk-engines plank gnome-control-center gnome-screensaver gnome-keyring seahorse openssh libsecret dconf-editor file-roller nautilus gvfs-google gnome-calculator eog eog-plugins evince rhythmbox qt5-base qt5ct kvantum-qt5 qbittorrent gstreamer gst-libav gst-plugins-base gst-plugins-good gst-plugins-ugly gst-plugins-bad gst-libav libva-vdpau-driver libva-utils vdpauinfo mpv youtube-dl rtmpdump ttf-hack adobe-source-code-pro-fonts noto-fonts noto-fonts-emoji ttf-dejavu ttf-liberation hunspell hunspell-es hunspell-en aspell aspell-es aspell-en wine winetricks virtualbox virtualbox-host-modules-arch python-nautilus libmythes mythes-en mythes-es hyphen hyphen-en hyphen-es linux-headers --needed --noconfirm
+sudo pacman -S xorg-xinit xorg-server xdg-user-dirs nvidia opencl-nvidia ocl-icd lib32-nvidia-utils lib32-opencl-nvidia lib32-ocl-icd firefox firefox-i18n-es-es perl-json-xs perl-term-readline-gnu pulseaudio qbittorrent gstreamer gst-libav gst-plugins-base gst-plugins-good gst-plugins-ugly gst-plugins-bad gst-libav libva-vdpau-driver libva-utils vdpauinfo mpv youtube-dl rtmpdump ttf-hack adobe-source-code-pro-fonts noto-fonts noto-fonts-emoji ttf-dejavu ttf-liberation hunspell hunspell-es hunspell-en aspell aspell-es aspell-en wine winetricks virtualbox virtualbox-host-modules-arch python-nautilus libmythes mythes-en mythes-es hyphen hyphen-en hyphen-es linux-headers --needed --noconfirm
 
 # Configure Xorg
 sudo localectl set-x11-keymap es 105
-echo 'QT_QPA_PLATFORMTHEME=qt5ct' | sudo tee --append /etc/environment
+#echo 'QT_QPA_PLATFORMTHEME=qt5ct' | sudo tee --append /etc/environment
 
 head -n -5 /etc/X11/xinit/xinitrc > ~/.xinitrc
 echo 'eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)' >> ~/.xinitrc
@@ -32,9 +32,9 @@ echo 'exec budgie-desktop' >> ~/.xinitrc
 echo '#!/bin/sh' > ~/.xserverrc
 echo 'exec /usr/bin/X -nolisten tcp -nolisten local "$@" vt$XDG_VTNR' >> ~/.xserverrc
 
-echo 'if [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then' >> ~/.bash_profile
-echo '    exec startx -- -keeptty > ~/.xorg.log 2>&1' >> ~/.bash_profile
-echo 'fi' >> ~/.bash_profile
+echo '#if [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then' >> ~/.bash_profile
+echo '#    exec startx -- -keeptty > ~/.xorg.log 2>&1' >> ~/.bash_profile
+echo '#fi' >> ~/.bash_profile
 
 sudo systemctl enable nvidia-persistenced
 
@@ -51,7 +51,7 @@ sudo modprobe vboxdrv
 mkdir /tmp/trizen && cd /tmp/trizen && git clone https://aur.archlinux.org/trizen.git
 cd trizen && makepkg -Ccirs --noconfirm --needed
 #gpg --keyserver pool.sks-keyservers.net --recv-keys 702353E0F7E48EDB 
-trizen -S megasync gnome-mpv tilix-bin virtualbox-ext-oracle visual-studio-code-bin --noconfirm --needed --noedit --noinfo
+#trizen -S virtualbox-ext-oracle visual-studio-code-bin --noconfirm --needed --noedit --noinfo
 
 #sudo systemctl enable vmware-networks.service
 #sudo systemctl enable vmware-usbarbitrator.service
@@ -62,14 +62,14 @@ trizen -S megasync gnome-mpv tilix-bin virtualbox-ext-oracle visual-studio-code-
 # sudo sed 's/%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/g' /tmp/sudoers.new > /tmp/sudoers.new
 # EDITOR='cp /tmp/sudoers.new' sudo visudo
 
-gsettings set org.gnome.desktop.interface gtk-theme "Arc-solid"
-gsettings set org.gnome.desktop.interface icon-theme "Papirus"
-gsettings set org.gnome.desktop.interface monospace-font-name "Source Code Pro 10"
-gsettings set org.gnome.desktop.interface document-font-name "Noto Sans 10"
-gsettings set org.gnome.desktop.interface font-name "Noto Sans 10"
-gsettings set org.gnome.desktop.wm.preferences titlebar-font "Noto Sans 10"
+#gsettings set org.gnome.desktop.interface gtk-theme "Arc-solid"
+#gsettings set org.gnome.desktop.interface icon-theme "Papirus"
+#gsettings set org.gnome.desktop.interface monospace-font-name "Source Code Pro 10"
+#gsettings set org.gnome.desktop.interface document-font-name "Noto Sans 10"
+#gsettings set org.gnome.desktop.interface font-name "Noto Sans 10"
+#gsettings set org.gnome.desktop.wm.preferences titlebar-font "Noto Sans 10"
 
 sudo sync
-sudo systemctl reboot
+#sudo systemctl reboot
 
 # END 20-firstboot.sh
